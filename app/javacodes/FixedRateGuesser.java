e1bc5f0082c4b7af7d0f58d61e09ffa870394dc4
Zhoujunxiu <junxiu.zhou.uol@gmail.com>
2014-07-31 23:22:32 +0100

    Fixed rate
    Fixed rate

--- app/javacodes/FixedRateGuesser.java
+++ app/javacodes/FixedRateGuesser.java
@@ -1,17 +1,44 @@
 package javacodes;
 
+import java.util.Date;
+
 public class FixedRateGuesser implements ColumnGuesser {
-
+	String regexRate = "[0-9]{0,2}\\.[0-9]+%?";
+	
 	@Override
-	public void getVal2Compare() {
+	public String getVal2Compare() {
 		// Select rate columns to be compared
-		
+   	  
+   	  	HTable table = new HTable(config, "duco_test");		    	  
+   	  
+   	  	String[] strArray=null;
+	    String ss;
+	    int count =0;
+   	  	while((ss=reader.readLine()) != null){
+   	  		strArray= ss.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"); 
+   	  		//splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+   		  
+   	  		for(int i=0; i< strArray.length; i++){
+   	  			strArray[i].trim();
+   	  		}
+   	  		
+   	  		String mRate="";
+   	  		for(int i=0; i< strArray.length; i++){
+   	  			if (strArray[i].matches(regexRate)){
+   	  				mRate = strArray[i];  
+   	  			}
+   	  		}return mRate;
+   	  	}
 	}
 
 	@Override
-	public String guessColumn() {
+	public int guessColumn(mRate) {
 		// Determine fixed rate between selected rates
-		return null;
+		for (int j=0; j< mRate.length; j++){
+		      if(mRate[j] > mRate[j+1])
+		    	  return mRate[j];
+		      else 
+		    	  return mRate[j+1];
+		}
 	}
-
 }
